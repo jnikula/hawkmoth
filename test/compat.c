b0170c46385ce84ddf908f0015cd67f4fa3dd744
Jani Nikula <jani@nikula.org>
2017-08-19 11:34:05 +0300

    test: add initial tests for compat conversions
    Have one symlinked source, and see the output differences based on
    arguments in the .stdin file.

---
+++ test/compat.c
@@ -0,0 +1,19 @@
+/**
+ * @param quick brown fox
+ * @return quick brown fox
+ * @random quick brown fox
+ */
+
+/**
+ * @param quick brown fox
+ * @return quick brown fox
+ * @random quick brown fox
+ */
+int foo(int quick);
+
+/**
+ * @quick: quick brown fox
+ * @return: quick brown fox
+ * @random: quick brown fox
+ */
+int kerneldoc(int quick);
