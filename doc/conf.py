de8056c511f9f7477279d0240fd8d1e92fd0d34f
Bruno Santos <brunomanuelsantos@tecnico.ulisboa.pt>
2018-12-01 20:40:48 +0100

    doc: make RTD theme dependency optional
    We still prefer the RTD theme, but we'll allow Sphinx to gracefully use
    the default theme du jour.

--- doc/conf.py
+++ doc/conf.py
@@ -130,7 +130,11 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+try:
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+except ImportError:
+    sys.stderr.write('Warning: Read The Docs theme is not available.\n')
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
