cdd01519e44c3066344f2577238dd399b7cd4b1e
Jani Nikula <jani@nikula.org>
2018-12-13 14:22:14 +0200

    packaging: add files for python packaging
    Also add some helpful build targets for packaging.

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 all:
 
-subdirs := test doc hawkmoth
+subdirs := . test doc hawkmoth
 
 include $(subdirs:%=%/Makefile.local)
 include Makefile.rules
---
+++ setup.py
@@ -0,0 +1,47 @@
+# Copyright (c) 2018, Jani Nikula <jani@nikula.org>
+# Licensed under the terms of BSD 2-Clause, see LICENSE for details.
+
+import setuptools
+
+with open('README.rst', 'r') as file:
+    long_description = file.read()
+
+setuptools.setup(
+    name = 'hawkmoth',
+    version = '0.1',
+    author = 'Jani Nikula',
+    author_email = 'jani@nikula.org',
+    license = '2-Clause BSD',
+    description = 'Hawkmoth - Sphinx Autodoc for C',
+    long_description = long_description,
+    long_description_content_type = 'text/x-rst',
+    url = 'https://github.com/jnikula/hawkmoth',
+    packages = [
+        'hawkmoth',
+    ],
+    install_requires = [
+        'sphinx',
+        # 'clang', # depend on distro packaging
+    ],
+    extras_requires = {
+        'dev': [
+            'sphinx-testing'
+        ],
+    },
+    python_requires = '~=3.4',
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Framework :: Sphinx :: Extension',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Documentation :: Sphinx',
+        'Topic :: Software Development :: Documentation',
+    ],
+    keywords = 'python sphinx autodoc documentation c',
+    project_urls = {
+        'Documentation': 'https://hawkmoth.readthedocs.io/en/latest/',
+        'Mailing List': 'https://www.freelists.org/list/hawkmoth',
+    }
+)
