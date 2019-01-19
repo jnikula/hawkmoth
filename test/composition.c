8818757a0f54749ba1142fc00d49d0b4e9f5d558
Bruno Santos <brunomanuelsantos@tecnico.ulisboa.pt>
2019-01-19 23:53:33 +0100

    test: add composition test
    Test the basic expectation on non-documentation comments between
    documentation comments and cursors detaching them from each other.
    
    [Jani: rewrite commit message]

---
+++ test/composition.c
@@ -0,0 +1,13 @@
+/** This is a file level comment. */
+
+/** This is a variable. */
+int a;
+
+/** This is another variable. */
+
+int b;
+
+/** This is another file level comment. */
+
+/* This comment breaks the docstr and variable association. */
+int c;
