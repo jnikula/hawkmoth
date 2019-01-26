28ac331e2b485274291db3340863fc85b91325f3
Jani Nikula <jani@nikula.org>
2019-01-26 16:34:20 +0200

    test: dynamically generate directive tests
    Similar to the parser test transition to dynamically generate test
    methods, but with more test separation added. After this, the Sphinx
    build is done independently for each test case, and separately for the
    actual directive output and the expected output. This removes any
    potential C domain interactions between test cases and expected/actual
    inputs.
    
    The change does mean the Sphinx build is run roughly 50x times per full
    test run, at the current number of test cases. This is somewhat offset
    by the ability to run individual directive test cases:
    
    $ test/test_cautodoc.py DirectiveTest.test_example_10_macro

--- test/test_cautodoc.py
+++ test/test_cautodoc.py
@@ -9,54 +9,35 @@ import unittest
 import testenv
 from sphinx_testing import with_app
 
+@with_app(confdir=testenv.testdir, create_new_srcdir=True, buildername='text')
+def _get_output(input_filename, app, status, warning, **options):
+    shutil.copyfile(input_filename,
+                    testenv.modify_filename(input_filename, dir=app.srcdir))
+
+    with open(os.path.join(app.srcdir, 'index.rst'), 'w') as file:
+        fmt = '.. c:autodoc:: {source}\n'
+        file.write(fmt.format(source=os.path.basename(input_filename)))
+        for key in options.keys():
+            fmt = '   :{key}: {value}\n'
+            file.write(fmt.format(key=key, value=options[key]))
+
+    app.build()
+
+    return testenv.read_file(os.path.join(app.outdir, 'index.txt'))
+
+@with_app(confdir=testenv.testdir, create_new_srcdir=True, buildername='text')
+def _get_expected(input_filename, app, status, warning, **options):
+    shutil.copyfile(testenv.modify_filename(input_filename, ext='stdout'),
+                    os.path.join(app.srcdir, 'index.rst'))
+
+    app.build()
+
+    return testenv.read_file(os.path.join(app.outdir, 'index.txt'))
+
 class DirectiveTest(unittest.TestCase):
+    pass
 
-    def _setup_src(self, srcdir, testcase_in):
-        testcase_out = testenv.modify_filename(testcase_in, dir=srcdir)
-
-        # use the pre-generated rst as comparison data
-        shutil.copyfile(testenv.modify_filename(testcase_in, ext='stdout'),
-                        testenv.modify_filename(testcase_out, ext='expected.rst'))
-
-        # set up an rst file to run the extension
-        shutil.copyfile(testcase_in, testcase_out)
-        options = testenv.get_testcase_options(testcase_in)
-
-        with open(testenv.modify_filename(testcase_out, ext='output.rst'), 'w') as file:
-            fmt = '.. c:autodoc:: {source}\n'
-            file.write(fmt.format(source=os.path.basename(testcase_out)))
-            for key in options.keys():
-                fmt = '   :{key}: {value}\n'
-                file.write(fmt.format(key=key, value=options[key]))
-
-    def _check_out(self, outdir, testcase_in):
-        testcase_out = testenv.modify_filename(testcase_in, dir=outdir)
-
-        # compare output from the pre-generated rst against the output generated
-        # by the extension
-
-        output = testenv.read_file(testenv.modify_filename(testcase_out,
-                                                           ext='output.txt'))
-
-        expected = testenv.read_file(testenv.modify_filename(testcase_out,
-                                                             ext='expected.txt'))
-
-        self.assertEqual(expected, output)
-
-    # Use copy_srcdir_to_tmpdir=False and outdir='some-dir' for debugging
-    @with_app(srcdir=os.path.join(testenv.testdir, 'sphinx'),
-              buildername='text', copy_srcdir_to_tmpdir=True)
-    def test_directive(self, app, status, warning):
-        testcases = list(testenv.get_testcases(testenv.testdir))
-
-        for f in testcases:
-            self._setup_src(app.srcdir, f)
-
-        app.build()
-
-        for f in testcases:
-            with self.subTest(source=os.path.basename(f)):
-                self._check_out(app.outdir, os.path.basename(f))
+testenv.assign_test_methods(DirectiveTest, _get_output, _get_expected)
 
 if __name__ == '__main__':
     unittest.main()
