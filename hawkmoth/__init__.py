f3375bdb798142cb58a994f9026e1e77d0a5aab3
Jani Nikula <jani@nikula.org>
2021-06-17 18:48:35 +0300

    cautodoc: subclass from SphinxDirective instead of Directive
    Sphinx provides a helper class SphinxDirective for Directive. Start
    subclassing SphinxDirective directly, and utilize the provided self.env
    to get at the env more directly.

--- hawkmoth/__init__.py
+++ hawkmoth/__init__.py
@@ -15,10 +15,10 @@ import subprocess
 import sys
 
 from docutils import nodes, statemachine
-from docutils.parsers.rst import directives, Directive
+from docutils.parsers.rst import directives
 from docutils.statemachine import ViewList
 from sphinx.util.nodes import nested_parse_with_titles
-from sphinx.util.docutils import switch_source_input
+from sphinx.util.docutils import switch_source_input, SphinxDirective
 from sphinx.util import logging
 
 from hawkmoth.parser import parse, ErrorLevel
@@ -28,7 +28,7 @@ with open(os.path.join(os.path.abspath(os.path.dirname(__file__)),
                        'VERSION')) as version_file:
     __version__ = version_file.read().strip()
 
-class CAutoDocDirective(Directive):
+class CAutoDocDirective(SphinxDirective):
     """Extract all documentation comments from the specified file"""
     required_argument = 1
     optional_arguments = 1
@@ -51,54 +51,46 @@ class CAutoDocDirective(Directive):
                 ErrorLevel.DEBUG: logging.LEVEL_NAMES['DEBUG']}
 
     def __display_parser_diagnostics(self, errors):
-        env = self.state.document.settings.env
-
         for (severity, filename, lineno, msg) in errors:
             if filename:
                 toprint = f'{filename}:{lineno}: {msg}'
             else:
                 toprint = f'{msg}'
 
-            if severity.value <= env.app.verbosity:
+            if severity.value <= self.env.app.verbosity:
                 self.logger.log(self._log_lvl[severity], toprint,
-                                location=(env.docname, self.lineno))
+                                location=(self.env.docname, self.lineno))
 
     def __get_clang_args(self):
-        env = self.state.document.settings.env
-
-        clang_args = env.config.cautodoc_clang.copy()
+        clang_args = self.env.config.cautodoc_clang.copy()
 
         clang_args.extend(strutil.args_as_list(self.options.get('clang')))
 
         return clang_args
 
     def __get_compat_transform(self):
-        env = self.state.document.settings.env
-
-        compat = self.options.get('compat', env.config.cautodoc_compat)
+        compat = self.options.get('compat', self.env.config.cautodoc_compat)
         if compat is None:
             return None
 
         fmt = 'cautodoc_compat and compat options are deprecated, please use cautodoc_transformations and transform options instead.'
-        self.logger.warning(fmt, location=(env.docname, self.lineno))
+        self.logger.warning(fmt, location=(self.env.docname, self.lineno))
 
         return lambda comment: doccompat.convert(comment, transform=compat)
 
     def __get_transform(self):
-        env = self.state.document.settings.env
-
         # Handle deprecated compat. To be removed.
         transform = self.__get_compat_transform()
         if transform is not None:
             return transform
 
-        transformations = env.config.cautodoc_transformations
+        transformations = self.env.config.cautodoc_transformations
         tropt = self.options.get('transform')
 
         if transformations is None:
             if tropt is not None:
                 self.logger.warning('transform specified without cautodoc_transformations config.',
-                                    location=(env.docname, self.lineno))
+                                    location=(self.env.docname, self.lineno))
 
             return None
 
@@ -106,15 +98,13 @@ class CAutoDocDirective(Directive):
         if tropt not in transformations:
             if tropt is not None:
                 self.logger.warning(f'unknown transformation "{tropt}".',
-                                    location=(env.docname, self.lineno))
+                                    location=(self.env.docname, self.lineno))
             return None
 
         # Note: None is a valid value for no transformation.
         return transformations.get(tropt)
 
     def __parse(self, viewlist, filename):
-        env = self.state.document.settings.env
-
         clang_args = self.__get_clang_args()
         transform = self.__get_transform()
 
@@ -131,26 +121,24 @@ class CAutoDocDirective(Directive):
                 lineoffset += 1
 
     def run(self):
-        env = self.state.document.settings.env
-
         result = ViewList()
 
         for pattern in self.arguments[0].split():
-            filenames = glob.glob(env.config.cautodoc_root + '/' + pattern)
+            filenames = glob.glob(self.env.config.cautodoc_root + '/' + pattern)
             if len(filenames) == 0:
                 self.logger.warning(f'Pattern "{pattern}" does not match any files.',
-                                    location=(env.docname, self.lineno))
+                                    location=(self.env.docname, self.lineno))
                 continue
 
             for filename in filenames:
                 mode = os.stat(filename).st_mode
                 if stat.S_ISDIR(mode):
                     self.logger.warning(f'Path "{filename}" matching pattern "{pattern}" is a directory.',
-                                        location=(env.docname, self.lineno))
+                                        location=(self.env.docname, self.lineno))
                     continue
 
                 # Tell Sphinx about the dependency and parse the file
-                env.note_dependency(os.path.abspath(filename))
+                self.env.note_dependency(os.path.abspath(filename))
                 self.__parse(result, filename)
 
         # Parse the extracted reST
