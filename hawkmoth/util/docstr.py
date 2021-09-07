0bae719efe165be5feba38bc1a495f522cd32bcf
Jani Nikula <jani@nikula.org>
2021-09-08 00:33:00 +0300

    comment: incorporate docstr.generate() in Comment.get_comment()
    Move the knowledge about documentation generation in the Comment class.

--- hawkmoth/util/docstr.py
+++ hawkmoth/util/docstr.py
@@ -11,7 +11,7 @@ construct.
 
 import re
 
-def _strip(comment):
+def strip(comment):
     """Strip comment from comment markers."""
     comment = re.sub(r'^/\*\*[ \t]?', '', comment)
     comment = re.sub(r'\*/$', '', comment)
@@ -39,35 +39,3 @@ def nest(text, nest):
         str: Indented reST documentation string.
     """
     return re.sub('(?m)^(?!$)', '   ' * nest, text)
-
-def generate(text, indent, fmt, name=None,
-             ttype=None, args=None, transform=None):
-    """
-    Generate reST documentation string.
-
-    Args:
-        text (str): Documentation body.
-        fmt (enum :py:class:`Type`): Format type to use. Different formats
-            require different arguments and ignores others if given.
-        name (str): Name of the documented token.
-        ttype (str): Type of the documented token.
-        args (list): List of arguments (str).
-        transform (func): Transformation function to be applied to the
-            documentation body. This is useful (e.g.) to extend the generator
-            with different syntaxes by converting them to reST. This is applied
-            on the documentation body after removing comment markers.
-
-    Returns:
-        str: reST documentation string.
-    """
-
-    text = _strip(text)
-
-    if transform is not None:
-        text = transform(text)
-
-    if args is not None:
-        args = ', '.join(args)
-
-    text = nest(text, indent)
-    return fmt.format(text=text, name=name, ttype=ttype, args=args)
