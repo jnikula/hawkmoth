a2a3ff46b39d439edd72d1116055414af45d32ae
Jani Nikula <jani@nikula.org>
2021-09-08 00:50:35 +0300

    docstring: add classes for all types of docstrings
    Encapsulate the docstring formats in dedicated classes.

--- hawkmoth/docstring.py
+++ hawkmoth/docstring.py
@@ -4,9 +4,10 @@
 from hawkmoth.util import docstr
 
 class Docstring():
-    def __init__(self, text, fmt=docstr.Type.TEXT, name=None, ttype=None, args=None, meta=None, nest=0):
+    _fmt = docstr.Type.TEXT
+
+    def __init__(self, text, name=None, ttype=None, args=None, meta=None, nest=0):
         self._text = text
-        self._fmt = fmt
         self._name = name
         self._ttype = ttype
         self._args = args
@@ -30,3 +31,36 @@ class Docstring():
 
     def get_line(self):
         return self._meta['line']
+
+class TextDocstring(Docstring):
+    _fmt = docstr.Type.TEXT
+
+class VarDocstring(Docstring):
+    _fmt = docstr.Type.VAR
+
+class TypeDocstring(Docstring):
+    _fmt = docstr.Type.TYPE
+
+class StructDocstring(Docstring):
+    _fmt = docstr.Type.STRUCT
+
+class UnionDocstring(Docstring):
+    _fmt = docstr.Type.UNION
+
+class EnumDocstring(Docstring):
+    _fmt = docstr.Type.ENUM
+
+class EnumeratorDocstring(Docstring):
+    _fmt = docstr.Type.ENUM_VAL
+
+class MemberDocstring(Docstring):
+    _fmt = docstr.Type.MEMBER
+
+class MacroDocstring(Docstring):
+    _fmt = docstr.Type.MACRO
+
+class MacroFunctionDocstring(Docstring):
+    _fmt = docstr.Type.MACRO_FUNC
+
+class FunctionDocstring(Docstring):
+    _fmt = docstr.Type.FUNC
