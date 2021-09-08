a98c2603ab019067a8aee39bbcd3ee8694537678
Jani Nikula <jani@nikula.org>
2021-09-08 10:50:18 +0300

    parser: generate documentation from comments on demand
    Store the raw C comment in the Comment class, and only generate the
    documentation when requested.

--- hawkmoth/comment.py
+++ hawkmoth/comment.py
@@ -1,13 +1,30 @@
 # Copyright (c) 2021 Jani Nikula <jani@nikula.org>
 # Licensed under the terms of BSD 2-Clause, see LICENSE for details.
 
+from hawkmoth.util import docstr
+
 class Comment():
-    def __init__(self, doc, meta):
-        self.doc = doc
+    def __init__(self, text, fmt=docstr.Type.TEXT, name=None, ttype=None, args=None, transform=None, meta=None, nest=0):
+        self.text = text
+        self.fmt = fmt
+        self.name = name
+        self.ttype = ttype
+        self.args = args
+        self.transform = transform
         self.meta = meta
+        self.nest = nest
 
     def get_rst(self):
-        return self.doc
+        # FIXME: docstr.generate changes the number of lines in output. This
+        # impacts the error reporting via meta['line']. Adjust meta to take this
+        # into account.
+
+        rst = docstr.generate(text=self.text, fmt=self.fmt, name=self.name, ttype=self.ttype,
+                              args=self.args, transform=self.transform)
+
+        rst = docstr.nest(rst, self.nest)
+
+        return rst
 
     def get_meta(self):
         return self.meta
