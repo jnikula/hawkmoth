bce1bc330c6e7044f4ba21a61a54729693fde265
Jani Nikula <jani@nikula.org>
2021-09-08 17:26:16 +0300

    test: add support for running extension tests using the html builder
    Using the html builder does not lose information as much as plain text,
    providing more accurate results. However, this is considerably slower
    than plain text, even if we switch to the basic html template for
    speed. To alleviate this, add a separate "full" test profile, and
    provide a 'make quick-test' target that skips the html tests.

--- test/test_cautodoc.py
+++ test/test_cautodoc.py
@@ -10,8 +10,7 @@ from sphinx_testing import with_app
 
 from test import testenv
 
-@with_app(confdir=testenv.testdir, create_new_srcdir=True, buildername='text')
-def _get_output(input_filename, app, status, warning, **options):
+def _get_output(input_filename, app, status, warning, output_suffix, **options):
     shutil.copyfile(input_filename,
                     testenv.modify_filename(input_filename, dir=app.srcdir))
 
@@ -23,18 +22,45 @@ def _get_output(input_filename, app, status, warning, **options):
 
     app.build()
 
-    return testenv.read_file(os.path.join(app.outdir, 'index.txt')), None
+    output_filename = os.path.join(app.outdir, f'index.{output_suffix}')
 
-@with_app(confdir=testenv.testdir, create_new_srcdir=True, buildername='text')
-def _get_expected(input_filename, app, status, warning, **options):
+    return testenv.read_file(output_filename), None
+
+def _get_expected(input_filename, app, status, warning, output_suffix, **options):
     shutil.copyfile(testenv.modify_filename(input_filename, ext='rst'),
                     os.path.join(app.srcdir, 'index.rst'))
 
     app.build()
 
-    return testenv.read_file(os.path.join(app.outdir, 'index.txt')), None
+    output_filename = os.path.join(app.outdir, f'index.{output_suffix}')
+
+    return testenv.read_file(output_filename), None
+
+# Test using Sphinx plain text builder
+@with_app(confdir=testenv.testdir, create_new_srcdir=True, buildername='text')
+def _get_output_text(input_filename, app, status, warning, **options):
+    return _get_output(input_filename, app, status, warning, 'txt', **options)
+
+@with_app(confdir=testenv.testdir, create_new_srcdir=True, buildername='text')
+def _get_expected_text(input_filename, app, status, warning, **options):
+    return _get_expected(input_filename, app, status, warning, 'txt', **options)
 
 @pytest.mark.parametrize('input_filename', testenv.get_testcases(testenv.testdir),
                          ids=testenv.get_testid)
-def test_directive(input_filename):
-    testenv.run_test(input_filename, _get_output, _get_expected)
+def test_directive_text(input_filename):
+    testenv.run_test(input_filename, _get_output_text, _get_expected_text)
+
+# Test using Sphinx html builder
+@with_app(confdir=testenv.testdir, create_new_srcdir=True, buildername='html')
+def _get_output_html(input_filename, app, status, warning, **options):
+    return _get_output(input_filename, app, status, warning, 'html', **options)
+
+@with_app(confdir=testenv.testdir, create_new_srcdir=True, buildername='html')
+def _get_expected_html(input_filename, app, status, warning, **options):
+    return _get_expected(input_filename, app, status, warning, 'html', **options)
+
+@pytest.mark.full
+@pytest.mark.parametrize('input_filename', testenv.get_testcases(testenv.testdir),
+                         ids=testenv.get_testid)
+def test_directive_html(input_filename):
+    testenv.run_test(input_filename, _get_output_html, _get_expected_html)
