4b7f4a9f54130d3989a8b8fc933a839669675e47
Jani Nikula <jani@nikula.org>
2021-09-09 21:22:42 +0300

    doc: update fallback automock code to match actual extension
    This has fallen behind.

--- doc/ext/automock/__init__.py
+++ doc/ext/automock/__init__.py
@@ -6,6 +6,9 @@ Automock
 
 Sphinx C Domain directive extension to mock Hawkmoth by reading the
 corresponding expected .rst test result instead of actual processing.
+
+This is not an example how to do documentation fallbacks for regular projects
+when Hawkmoth is not available.
 """
 
 import os
@@ -14,13 +17,11 @@ from docutils.parsers.rst import directives
 from sphinx.directives.other import Include
 
 class Automock(Include):
-    required_argument = 1
-    optional_arguments = 1
-
-    # Allow passing a variable number of file patterns as arguments
-    final_argument_whitespace = True
+    required_arguments = 1
+    optional_arguments = 100 # arbitrary limit
 
     option_spec = {
+        'transform': directives.unchanged_required,
         'compat': directives.unchanged_required,
         'clang': directives.unchanged_required,
     }
@@ -34,9 +35,10 @@ class Automock(Include):
         return super(Include, self).run()
 
 def setup(app):
-    app.add_config_value('cautodoc_root', app.confdir, 'env')
-    app.add_config_value('cautodoc_compat', None, 'env')
-    app.add_config_value('cautodoc_clang', None, 'env')
+    app.add_config_value('cautodoc_root', app.confdir, 'env', [str])
+    app.add_config_value('cautodoc_compat', None, 'env', [str])
+    app.add_config_value('cautodoc_transformations', None, 'env', [dict])
+    app.add_config_value('cautodoc_clang', [], 'env', [list])
     app.add_directive_to_domain('c', 'autodoc', Automock)
 
     return dict(version = '0',
