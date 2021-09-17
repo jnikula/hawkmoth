18fa29068c3cdc34d88e898c30797439851b36fe
Jani Nikula <jani@nikula.org>
2021-09-17 18:44:45 +0300

    cautodoc: let docutils do the directive option conversion for clang args
    The option_spec provides a way to convert the option argument. Turn
    args_as_list into a conversion function string_list, and let docutils
    call it.
    
    Docutils documentation:
    https://docutils.sourceforge.io/docs/howto/rst-directives.html#option-conversion-functions

--- hawkmoth/util/strutil.py
+++ hawkmoth/util/strutil.py
@@ -1,9 +1,8 @@
 # Copyright (c) 2021, Jani Nikula <jani@nikula.org>
 # Licensed under the terms of BSD 2-Clause, see LICENSE for details.
 
-def args_as_list(args):
-    if isinstance(args, str):
-        args = [s.strip() for s in args.split(',') if len(s.strip()) > 0]
-    if args is None:
-        args = []
-    return args
+def string_list(argument):
+    if argument is None:
+        return []
+
+    return [s.strip() for s in argument.split(',') if len(s.strip()) > 0]
