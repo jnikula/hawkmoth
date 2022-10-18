a756e59b35fa3e264a6628387caa971e573458d2
Robin Bourianes <robin.bourianes@kalisio.com>
2022-10-18 23:09:40 +0200

    test: added more function pointer variable test cases
    FIXME: the output for "*const *volatile *restrict" isn't right
    
    [Jani: whitespace changes to reflect code changes.]

--- test/variable.c
+++ test/variable.c
@@ -9,6 +9,11 @@ static int sheesh;
 int (*function_pointer_variable)(int *param_name_ignored);
 
 /**
+ * variadic function pointer variable
+ */
+int (*variadic_function_pointer_variable)(int *param_name_ignored, ...);
+
+/**
  * pointer to function pointer variable
  */
 int (**pointer_to_function_pointer_variable)(int);
@@ -19,9 +24,29 @@ int (**pointer_to_function_pointer_variable)(int);
 void (*function_pointer_array[5])(void);
 
 /**
+ * array of array of function pointers
+ */
+void (*array_of_function_pointer_array[5][15])(void);
+
+/**
  * function pointer with lots of const qualifiers
  */
 const char* (*const function_pointer_with_qualifier)(const char *in);
+
+/**
+ * function pointer with multiple qualifiers
+ */
+const char* (*const volatile function_pointer_with_multiple_qualifiers)(const char *in);
+
+/**
+ * a complex type involving function pointers somehow
+ */
+const char* (*const *volatile *restrict legal_type_involving_function_pointer[12][3])(const char *in);
+
+/**
+ * function pointer to a function taking a function pointer as arg
+ */
+int (*function_pointer_with_function_pointer_arg)(float (*arg1)(char c));
 
 /**
  * Array of pointers.
