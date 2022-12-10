7191f97bb2ab954f19c8f7d8c1474307ddfc2cf0
Jani Nikula <jani@nikula.org>
2022-12-10 21:10:48 +0200

    examples: add an introductory example using c:autodoc
    Add an overview example first using c:autodoc. Drop the now redundant
    generic comment example.

---
+++ test/example-01-autodoc.c
@@ -0,0 +1,58 @@
+/**
+ * The ``c:autodoc`` directive is the easiest way to extract all the
+ * documentation comments from one or more source files in one go. The
+ * other directives provide more fine-grained control over what to
+ * document.
+ *
+ * This example provides a brief overview of the most common features.
+ *
+ * Note that the documentation comments below are **not** good examples
+ * of how to document your code. Instead, the comments primarily
+ * describe the features of Hawkmoth and Sphinx.
+ *
+ * Source files may contain documentation comments not attached to any C
+ * constructs. They will be included as generic documentation comments,
+ * like this one.
+ */
+
+/**
+ * Macro documentation.
+ */
+#define ERROR -1
+
+/**
+ * Struct documentation.
+ */
+struct foo {
+	/**
+	 * Member documentation.
+	 */
+	const char *m1;
+	/**
+	 * Member documentation.
+	 */
+	int m2;
+};
+
+/**
+ * Enum documentation.
+ */
+enum bar {
+	/**
+	 * Enumeration constant documentation.
+	 */
+	E1,
+	/**
+	 * Enumeration constant documentation.
+	 */
+	E2,
+};
+
+/**
+ * Function documentation.
+ *
+ * :param p1: Parameter documentation
+ * :param int p2: Parameter documentation with type
+ * :return: Return value documentation
+ */
+int baz(int p1, int p2);
