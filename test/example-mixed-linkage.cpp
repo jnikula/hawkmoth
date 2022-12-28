f5a968992f1f1288e8d36e91a8f264830890829b
Bruno Santos <brunomanuelsantos@tecnico.ulisboa.pt>
2022-12-28 22:45:44 +0000

    test: add C/C++ language detection tests
    Currently C++ specific cursor types are not handled so these tests will
    also evolve as support is added.

---
+++ test/example-mixed-linkage.cpp
@@ -0,0 +1,15 @@
+/** C++ function. */
+int foo(void);
+
+/** Top level docstring 1. */
+extern "C" {
+
+/** Top level docstring 2. */
+
+/** C function. */
+int bar(void);
+
+}
+
+/** Another C++ function. */
+int baz(void);
