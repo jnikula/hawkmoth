80c001e68a1512514384d16c7a866fc0b6b929ff
Jani Nikula <jani@nikula.org>
2023-01-08 21:38:20 +0200

    extensions: move cautodoc_transformations handling to an extension
    Transformations via cautodoc_transformations will be deprecated in some
    future release of Hawkmoth in favor of using hawkmoth-process-docstring
    event.
    
    However, we can already move cautodoc_transformations handling to an
    extension that is, for now, setup by default. In the future, we can
    still keep the extension, but have users explicitly set it up in conf.py
    if they need it. And later on, we can remove the extension from Hawkmoth
    altogether.
    
    This also marks the beginning of built-in extensions to Hawkmoth in
    hawkmoth.ext.

---
+++ hawkmoth/ext/transformations/__init__.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2023, Jani Nikula <jani@nikula.org>
+# Licensed under the terms of BSD 2-Clause, see LICENSE for details.
+
+def _process_docstring(app, lines, transform, options):
+    transformations = app.config.cautodoc_transformations
+    tropt = options.get('transform')
+
+    if transformations is None:
+        return
+
+    # Note: None is a valid key for default.
+    if tropt not in transformations:
+        return
+
+    # Note: None is a valid value for no transformation.
+    transform = transformations.get(tropt)
+    if transform is None:
+        return
+
+    comment = '\n'.join(lines)
+    comment = transform(comment)
+    lines[:] = comment.splitlines()[:]
+
+def setup(app):
+    app.add_config_value('cautodoc_transformations', None, 'env', [dict])
+
+    # Run before event handlers with default priority.
+    app.connect('hawkmoth-process-docstring', _process_docstring, 400)
+
+    return {
+        'parallel_read_safe': True,
+    }
