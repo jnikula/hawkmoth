86057c9301461a87fa646d92400209cef3e08f11
Jani Nikula <jani@nikula.org>
2023-01-13 22:40:17 +0200

    test: add ExtensionTestcase class
    Add base class for ExtensionTextTestcase and ExtensionHtmlTestcase
    between them and Testcase class to handle shared code.

--- test/test_cautodoc.py
+++ test/test_cautodoc.py
@@ -13,59 +13,60 @@ from test import testenv
 def _get_suffix(buildername):
     return 'txt' if buildername == 'text' else buildername
 
-def _get_output(testcase, app, status, warning):
-    input_filename = testcase.get_input_filename()
-    shutil.copyfile(input_filename,
-                    os.path.join(app.srcdir, os.path.basename(input_filename)))
+class ExtensionTestcase(testenv.Testcase):
+    def get_output(self, app, status, warning):
+        input_filename = self.get_input_filename()
+        shutil.copyfile(input_filename,
+                        os.path.join(app.srcdir, os.path.basename(input_filename)))
 
-    directive_str = testcase.get_directive_string()
+        directive_str = self.get_directive_string()
 
-    with open(os.path.join(app.srcdir, 'index.rst'), 'w') as f:
-        f.write(directive_str)
+        with open(os.path.join(app.srcdir, 'index.rst'), 'w') as f:
+            f.write(directive_str)
 
-    # Hack: It's not possible to disable search via configuration
-    app.builder.search = False
+        # Hack: It's not possible to disable search via configuration
+        app.builder.search = False
 
-    app.build()
+        app.build()
 
-    output_suffix = _get_suffix(app.builder.name)
-    output_filename = os.path.join(app.outdir, f'index.{output_suffix}')
+        output_suffix = _get_suffix(app.builder.name)
+        output_filename = os.path.join(app.outdir, f'index.{output_suffix}')
 
-    return testenv.read_file(output_filename), None
+        return testenv.read_file(output_filename), None
 
-def _get_expected(testcase, app, status, warning):
-    shutil.copyfile(testcase.get_expected_filename(),
-                    os.path.join(app.srcdir, 'index.rst'))
+    def get_expected(self, app, status, warning):
+        shutil.copyfile(self.get_expected_filename(),
+                        os.path.join(app.srcdir, 'index.rst'))
 
-    # Hack: It's not possible to disable search via configuration
-    app.builder.search = False
+        # Hack: It's not possible to disable search via configuration
+        app.builder.search = False
 
-    app.build()
+        app.build()
 
-    output_suffix = _get_suffix(app.builder.name)
-    output_filename = os.path.join(app.outdir, f'index.{output_suffix}')
+        output_suffix = _get_suffix(app.builder.name)
+        output_filename = os.path.join(app.outdir, f'index.{output_suffix}')
 
-    return testenv.read_file(output_filename), None
+        return testenv.read_file(output_filename), None
 
 # Test using Sphinx plain text builder
-class ExtensionTextTestcase(testenv.Testcase):
+class ExtensionTextTestcase(ExtensionTestcase):
     @with_app(confdir=testenv.testdir, create_new_srcdir=True, buildername='text')
     def get_output(self, app, status, warning):
-        return _get_output(self, app, status, warning)
+        return super().get_output(app, status, warning)
 
     @with_app(confdir=testenv.testdir, create_new_srcdir=True, buildername='text')
     def get_expected(self, app, status, warning):
-        return _get_expected(self, app, status, warning)
+        return super().get_expected(app, status, warning)
 
 # Test using Sphinx html builder
-class ExtensionHtmlTestcase(testenv.Testcase):
+class ExtensionHtmlTestcase(ExtensionTestcase):
     @with_app(confdir=testenv.testdir, create_new_srcdir=True, buildername='html')
     def get_output(self, app, status, warning):
-        return _get_output(self, app, status, warning)
+        return super().get_output(app, status, warning)
 
     @with_app(confdir=testenv.testdir, create_new_srcdir=True, buildername='html')
     def get_expected(self, app, status, warning):
-        return _get_expected(self, app, status, warning)
+        return super().get_expected(app, status, warning)
 
 def _get_text_testcases(path):
     for f in testenv.get_testcase_filenames(path):
