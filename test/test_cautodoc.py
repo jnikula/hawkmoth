aad49578c04b5d69848e3ee009cb39a73dcc0184
Jani Nikula <jani@nikula.org>
2023-01-19 20:25:23 +0200

    test: drop the output file suffix parameter
    We can deduce it from the builder name.

--- test/test_cautodoc.py
+++ test/test_cautodoc.py
@@ -10,7 +10,10 @@ from sphinx_testing import with_app
 
 from test import testenv
 
-def _get_output(testcase, app, status, warning, output_suffix):
+def _get_suffix(buildername):
+    return 'txt' if buildername == 'text' else buildername
+
+def _get_output(testcase, app, status, warning):
     input_filename = testcase.get_input_filename()
     shutil.copyfile(input_filename,
                     os.path.join(app.srcdir, os.path.basename(input_filename)))
@@ -25,11 +28,12 @@ def _get_output(testcase, app, status, warning, output_suffix):
 
     app.build()
 
+    output_suffix = _get_suffix(app.builder.name)
     output_filename = os.path.join(app.outdir, f'index.{output_suffix}')
 
     return testenv.read_file(output_filename), None
 
-def _get_expected(testcase, app, status, warning, output_suffix):
+def _get_expected(testcase, app, status, warning):
     shutil.copyfile(testcase.get_expected_filename(),
                     os.path.join(app.srcdir, 'index.rst'))
 
@@ -38,6 +42,7 @@ def _get_expected(testcase, app, status, warning, output_suffix):
 
     app.build()
 
+    output_suffix = _get_suffix(app.builder.name)
     output_filename = os.path.join(app.outdir, f'index.{output_suffix}')
 
     return testenv.read_file(output_filename), None
@@ -59,11 +64,11 @@ def _get_html_testcases(path):
 # Test using Sphinx plain text builder
 @with_app(confdir=testenv.testdir, create_new_srcdir=True, buildername='text')
 def _get_output_text(testcase, app, status, warning, **unused):
-    return _get_output(testcase, app, status, warning, 'txt')
+    return _get_output(testcase, app, status, warning)
 
 @with_app(confdir=testenv.testdir, create_new_srcdir=True, buildername='text')
 def _get_expected_text(testcase, app, status, warning, **unused):
-    return _get_expected(testcase, app, status, warning, 'txt')
+    return _get_expected(testcase, app, status, warning)
 
 @pytest.mark.parametrize('testcase', _get_text_testcases(testenv.testdir),
                          ids=testenv.get_testid)
@@ -73,11 +78,11 @@ def test_directive_text(testcase):
 # Test using Sphinx html builder
 @with_app(confdir=testenv.testdir, create_new_srcdir=True, buildername='html')
 def _get_output_html(testcase, app, status, warning, **unused):
-    return _get_output(testcase, app, status, warning, 'html')
+    return _get_output(testcase, app, status, warning)
 
 @with_app(confdir=testenv.testdir, create_new_srcdir=True, buildername='html')
 def _get_expected_html(testcase, app, status, warning, **unused):
-    return _get_expected(testcase, app, status, warning, 'html')
+    return _get_expected(testcase, app, status, warning)
 
 @pytest.mark.full
 @pytest.mark.parametrize('testcase', _get_html_testcases(testenv.testdir),
