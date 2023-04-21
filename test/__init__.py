6fe267dcffcb2f1b9cfb5f8d6a05ccd55128be69
Bruno Santos <brunomanuelsantos@tecnico.ulisboa.pt>
2023-04-21 11:24:23 +0100

    test: add our own src/ directory to the search path
    As an artefact of the recent move of the module into the src/ folder,
    the unit tests cannot find the local package anylonger. Worse still,
    with a sufficiently compatible version of hawkmoth available in the
    system path, we may be duped into thinking the tests are working when
    they aren't.

--- test/__init__.py
+++ test/__init__.py
@@ -2,6 +2,11 @@
 # Licensed under the terms of BSD 2-Clause, see LICENSE for details.
 
 import pytest
+import os
+import sys
+
+# Use 'our' version of the module.
+sys.path.insert(0, os.path.abspath('src'))
 
 # Enable introspection on assertions in testenv
 pytest.register_assert_rewrite('test.testenv')
