a1ecfb61fae9ec869dc12361ddeb6a93b83e721f
Jani Nikula <jani@nikula.org>
2023-07-02 15:23:10 +0300

    test: use process_docstring naming instead of transform
    Follow the hawkmoth-process-docstring event naming.

--- test/test_parser.py
+++ test/test_parser.py
@@ -21,7 +21,7 @@ parser_transformations = {
     'javadoc': doccompat.javadoc_liberal,
 }
 
-def _transform(transform, lines):
+def _process_docstring(transform, lines):
     if transform:
         text = '\n'.join(lines)
         text = transform(text)
@@ -91,10 +91,12 @@ class ParserTestcase(testenv.Testcase):
         else:
             transform = None
 
+        process_docstring = lambda lines: _process_docstring(transform, lines)
+
         for docstrings in root.walk(recurse=False, filter_types=_filter_types(directive),
                                     filter_names=_filter_names(directive, options)):
             for docstr in docstrings.walk(filter_names=_filter_members(directive, directive_options)):  # noqa: E501
-                lines = docstr.get_docstring(process_docstring=lambda lines: _transform(transform, lines))
+                lines = docstr.get_docstring(process_docstring=process_docstring)
                 docs_str += '\n'.join(lines) + '\n'
 
         for error in errors:
