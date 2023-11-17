1b3000b3e9673674ddfa95749fb3af16f6d3d0ec
Jani Nikula <jani@nikula.org>
2023-11-17 12:29:22 +0200

    examples: update the javadoc/doxygen example
    Improve the description of the javadoc builtin extension. This also
    expands the tests slightly.

--- test/examples/javadoc.c
+++ test/examples/javadoc.c
@@ -8,11 +8,25 @@ enum mode;
  * Sphinx event. You can use the built-in extensions for this, or create your
  * own.
  *
- * In this example, hawkmoth.ext.javadoc built-in extension is used to support
- * Javadoc-style documentation comments.
+ * In this example, <tt>hawkmoth.ext.javadoc</tt> built-in extension is used to
+ * support Javadoc/Doxygen-style documentation comments. You can use both \@ and
+ * \\ for the commands.
+ *
+ * \note
+ * While the most common commands and inline markup \a should work, the
+ * Javadoc/Doxygen support is nowhere near complete.
+ *
+ * The support should be good enough for basic API documentation, including
+ * things like code blocks:
+ *
+ * \code
+ * ¯\_(ツ)_/¯
+ * \endcode
+ *
+ * And parameter and return value descriptions, and the like:
  *
  * @param list The list to frob.
- * @param mode The frobnication mode.
+ * @param[in] mode The frobnication mode.
  * @return 0 on success, non-zero error code on error.
  * @since v0.1
  */
--- test/examples/javadoc.rst
+++ test/examples/javadoc.rst
@@ -7,13 +7,31 @@
    Sphinx event. You can use the built-in extensions for this, or create your
    own.
 
-   In this example, hawkmoth.ext.javadoc built-in extension is used to support
-   Javadoc-style documentation comments.
+   In this example, ``hawkmoth.ext.javadoc`` built-in extension is used to
+   support Javadoc/Doxygen-style documentation comments. You can use both \@ and
+   \\ for the commands.
+
+
+   .. note::
+
+      While the most common commands and inline markup *should* work, the
+      Javadoc/Doxygen support is nowhere near complete.
+
+   The support should be good enough for basic API documentation, including
+   things like code blocks:
+
+
+   .. code-block::
+
+      ¯\_(ツ)_/¯
+
+
+   And parameter and return value descriptions, and the like:
 
 
    :param list: The list to frob.
 
-   :param mode: The frobnication mode.
+   :param mode: **[in]** The frobnication mode.
 
    :return: 0 on success, non-zero error code on error.
 
