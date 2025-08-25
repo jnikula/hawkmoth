a204fc97a93902c7c3e8aa12d864c810ecb7dc86
Evan Blum <evan.blum@virtuososurgical.net>
2025-08-25 15:08:11 -0500

    trailing-comments: Restructure the comment extraction function
    Restructure the _comment_extract function to first determine the token type (i.e.. comment, documentable construct, or something else that either does or doesn't break the comment context), and then extract comments based on the token type that was determined. This just separates the logic for grouping comments from the logic that decides what's an applicable comment.

--- src/hawkmoth/parser.py
+++ src/hawkmoth/parser.py
@@ -147,52 +147,93 @@ def _comment_extract(tu, errors):
 
     top_level_comments = []
     comments = {}
-    current_comment = None
+    current_leading_comment = None
 
     def is_leading_doc(cursor):
         return cursor and docstring.Docstring.is_leading_doc(cursor.spelling)
 
+    class TokenType(enum.Enum):
+        """
+        Token types used in the parser
+        """
+
+        LEADING_COMMENT = enum.auto()
+        SKIPPABLE = enum.auto()
+        DOCUMENTABLE = enum.auto()
+        SEPARATOR = enum.auto()
+        UNKNOWN = enum.auto()
+
     for token in tu.get_tokens(extent=tu.cursor.extent):
-        # Handle all comments we come across.
-        if token.kind == TokenKind.COMMENT:
-            # If we already have a comment, it wasn't related to another cursor.
-            if is_leading_doc(current_comment):
-                top_level_comments.append(current_comment)
-            current_comment = token
-            continue
+        token_type = TokenType.UNKNOWN
 
         # Store off the token's cursor for a slight performance improvement
         # instead of accessing the `cursor` property multiple times.
         token_cursor = token.cursor
 
+        # Semicolons and commas belong to cursor of the outer scope.  We want
+        # to be able to add trailing comments after the semicolon on the line,
+        # so skip them here.
+        if token.kind == TokenKind.PUNCTUATION and (
+            token.spelling == ";" or token.spelling == ","
+        ):
+            token_type = TokenType.SKIPPABLE
+
+        elif token_cursor.kind in [CursorKind.INTEGER_LITERAL,
+                                   CursorKind.FLOATING_LITERAL,
+                                   CursorKind.IMAGINARY_LITERAL,
+                                   CursorKind.STRING_LITERAL,
+                                   CursorKind.CHARACTER_LITERAL, ]:
+            token_type = TokenType.SKIPPABLE
+
+        # Handle all comments we come across.
+        elif token.kind == TokenKind.COMMENT:
+            if is_leading_doc(token):
+                token_type = TokenType.LEADING_COMMENT
+            else:
+                token_type = TokenType.SEPARATOR
+
         # Cursors that are 1) never documented themselves, and 2) allowed
         # between the comment and the actual cursor being documented.
-        if token_cursor.kind in [CursorKind.INVALID_FILE,
-                                 CursorKind.TYPE_REF,
-                                 CursorKind.TEMPLATE_REF,
-                                 CursorKind.NAMESPACE_REF,
-                                 CursorKind.PREPROCESSING_DIRECTIVE,
-                                 CursorKind.MACRO_INSTANTIATION]:
-            continue
+        elif token_cursor.kind in [
+            CursorKind.INVALID_FILE,
+            CursorKind.TYPE_REF,
+            CursorKind.TEMPLATE_REF,
+            CursorKind.NAMESPACE_REF,
+            CursorKind.PREPROCESSING_DIRECTIVE,
+            CursorKind.MACRO_INSTANTIATION,
+        ]:
+            token_type = TokenType.SKIPPABLE
 
         # Cursors that are 1) never documented themselves, and 2) not allowed
         # between the comment and the actual cursor being documented.
-        if token_cursor.kind in [CursorKind.LINKAGE_SPEC,
-                                 CursorKind.UNEXPOSED_DECL]:
-            if is_leading_doc(current_comment):
-                top_level_comments.append(current_comment)
-            current_comment = None
-            continue
+        elif token_cursor.kind in [CursorKind.LINKAGE_SPEC, CursorKind.UNEXPOSED_DECL]:
+            token_type = TokenType.SEPARATOR
 
-        # Otherwise the current comment documents _this_ cursor. I.e.: not a top
-        # level comment.
-        if is_leading_doc(current_comment):
-            comments[token_cursor.hash] = current_comment
-        current_comment = None
+        # All other cursors can be documented
+        else:
+            token_type = TokenType.DOCUMENTABLE
 
-    # Comment at the end of file.
-    if is_leading_doc(current_comment):
-        top_level_comments.append(current_comment)
+        if token_type == TokenType.LEADING_COMMENT:
+            if current_leading_comment is not None:
+                top_level_comments.append(current_leading_comment)
+            current_leading_comment = token
+
+        elif token_type == TokenType.SEPARATOR:
+            if current_leading_comment is not None:
+                top_level_comments.append(current_leading_comment)
+            current_leading_comment = None
+
+        elif token_type == TokenType.SKIPPABLE:
+            pass  # Do nothing
+        elif token_type == TokenType.DOCUMENTABLE:
+            # If we have a leading comment, it applies to this cursor.
+            if current_leading_comment is not None:
+                comments[token_cursor.hash] = current_leading_comment
+                current_leading_comment = None
+
+    # Unattached comment at the end of file.
+    if current_leading_comment is not None:
+        top_level_comments.append(current_leading_comment)
 
     return top_level_comments, comments
 
